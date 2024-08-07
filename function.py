6518f1fc015f70063b78b4c0bfa1de046b6d78e4
Ulysse Renzetti <ulysse.renzetti@outlook.fr>
2024-08-07 14:42:11 +0200

    change file

---
+++ function.py
@@ -0,0 +1,11 @@
+from math import *
+
+
+def classique(a, b):
+    result = a * a + b * b
+    print(sqrt(result))
+
+
+def classique2(a, b):
+    result = a*a-b*b
+    print(sqrt(result))
--- main.py
+++ main.py
@@ -1,18 +1,21 @@
-def classique(a, b):
-    result=a*a+b*b
-    print(result)
+from math import *
+import function
 
 
 def main():
     mainchoice = input('Faites votre choix 1) Classique 2) Réciproque'
                        )
     if mainchoice == '1':
-        secondchoice =input('longueur à calculer : 1) hypothénuse 2) coté a 2) coté b')
+        secondchoice = input('longueur à calculer : 1) hypothénuse 2) coté autre')
+        if secondchoice == '2':
+            hypothenuse: int = int(input('coté1'))
+            cote2: int = int(input('cote2'))
+            function.classique2(hypothenuse,cote2)
 
         if secondchoice == '1':
-            cote1 : int= int(input('coté1'))
-            cote2: int=int(input('cote2'))
-            classique(cote1,cote2)
+            cote1: int = int(input('coté1'))
+            cote2: int = int(input('cote2'))
+            function.classique(cote1, cote2)
 
 
 main()

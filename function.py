7186ac10bc6ccb52418a3180f07029aaeb66c074
ski232323 <152718343+ski232323@users.noreply.github.com>
2024-10-27 14:42:15 +0100

    Update function.py

--- function.py
+++ function.py
@@ -1,18 +1,13 @@
 from math import *
-import web_pdb;
-
-web_pdb.set_trace()
-
 
 def classique(a, b):
     result = a * a + b * b
-    print(sqrt(result))
-
+    return sqrt(result)
 
 def classique2(a, b):
     result = a * a - b * b
-    print(sqrt(result))
-
+    return sqrt(result)
 
 def reciproque(a, b):
     result = (a * a) + (b * b)
+    return result
